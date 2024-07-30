4bba31294f053cea335f09cdd2e422437625c085
Iamouma <stephenouma452@gmail.com>
2024-07-30 10:23:27 +0000

    A basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A basic flask app
+"""
+
+
+from flask import FRlask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """
+    Returns an html file
+    """
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
